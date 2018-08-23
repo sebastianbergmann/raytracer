ec05f509d5b0c108e3c637cbb3f69348acd44a54
Sebastian Bergmann <sb@sebastian-bergmann.de>
2018-08-23 12:25:59 +0200

    Refactor

--- tests/CanvasTest.php
+++ tests/CanvasTest.php
@@ -12,25 +12,23 @@ final class CanvasTest extends TestCase
 {
     public function test_is_initially_black(): void
     {
-        $c = Canvas::create(10, 20, Color::create(0.0, 0.0, 0.0));
+        $canvas = Canvas::create(10, 20, Color::create(0.0, 0.0, 0.0));
+        $black  = Color::create(0.0, 0.0, 0.0);
 
         for ($x = 1; $x <= 10; $x++) {
             for ($y = 1; $y <= 20; $y++) {
-                $this->assertSame(0.0, $c->pixelAt($x, $y)->red());
-                $this->assertSame(0.0, $c->pixelAt($x, $y)->green());
-                $this->assertSame(0.0, $c->pixelAt($x, $y)->blue());
+                $this->assertTrue($canvas->pixelAt($x, $y)->equalTo($black));
             }
         }
     }
 
     public function test_a_pixel_can_be_written_to_a_canvas(): void
     {
-        $c = Canvas::create(10, 20, Color::create(0.0, 0.0, 0.0));
+        $canvas = Canvas::create(10, 20, Color::create(0.0, 0.0, 0.0));
+        $red    = Color::create(1.0, 0.0, 0.0);
 
-        $c->writePixel(2, 3, Color::create(1.0, 0.0, 0.0));
+        $canvas->writePixel(2, 3, $red);
 
-        $this->assertSame(1.0, $c->pixelAt(2, 3)->red());
-        $this->assertSame(0.0, $c->pixelAt(2, 3)->green());
-        $this->assertSame(0.0, $c->pixelAt(2, 3)->blue());
+        $this->assertTrue($canvas->pixelAt(2, 3)->equalTo($red));
     }
 }
