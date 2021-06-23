1bdc21d57d5b935259aac77f35f43f51fa6685e0
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-06-23 14:41:39 +0200

    A ray intersects a sphere at two points A ray intersects a sphere at a tangent A ray misses a sphere A ray originates inside a sphere A sphere is behind a ray

--- src/Sphere.php
+++ src/Sphere.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 namespace SebastianBergmann\Raytracer;
 
+use function sqrt;
+
 final class Sphere
 {
     private Tuple $origin;
@@ -26,5 +28,28 @@ final class Sphere
     public function radius(): float
     {
         return $this->radius;
+    }
+
+    /**
+     * @psalm-return list<float>
+     */
+    public function intersect(Ray $r): array
+    {
+        $sphereToRay = $r->origin()->minus($this->origin);
+
+        $a = $r->direction()->dot($r->direction());
+        $b = 2 * $r->direction()->dot($sphereToRay);
+        $c = $sphereToRay->dot($sphereToRay) - 1;
+
+        $discrimiant = $b ** 2 - 4 * $a * $c;
+
+        if ($discrimiant < 0) {
+            return [];
+        }
+
+        $t1 = (-$b - sqrt($discrimiant)) / (2 * $a);
+        $t2 = (-$b + sqrt($discrimiant)) / (2 * $a);
+
+        return [$t1, $t2];
     }
 }
--- tests/unit/SphereTest.php
+++ tests/unit/SphereTest.php
@@ -6,6 +6,8 @@ use PHPUnit\Framework\TestCase;
 /**
  * @covers \SebastianBergmann\Raytracer\Sphere
  *
+ * @uses \SebastianBergmann\Raytracer\Ray
+ *
  * @small
  */
 final class SphereTest extends TestCase
@@ -19,5 +21,98 @@ final class SphereTest extends TestCase
 
         $this->assertSame($origin, $s->origin());
         $this->assertSame($radius, $s->radius());
+    }
+
+    public function test_a_ray_intersects_a_sphere_at_two_points(): void
+    {
+        $r = Ray::from(
+            Tuple::point(0, 0, -5),
+            Tuple::vector(0, 0, 1)
+        );
+
+        $s = Sphere::from(
+            Tuple::point(0, 0, 0),
+            1.0
+        );
+
+        $xs = $s->intersect($r);
+
+        $this->assertCount(2, $xs);
+        $this->assertSame(4.0, $xs[0]);
+        $this->assertSame(6.0, $xs[1]);
+    }
+
+    public function test_a_ray_intersects_a_sphere_at_a_tangent(): void
+    {
+        $r = Ray::from(
+            Tuple::point(0, 1, -5),
+            Tuple::vector(0, 0, 1)
+        );
+
+        $s = Sphere::from(
+            Tuple::point(0, 0, 0),
+            1.0
+        );
+
+        $xs = $s->intersect($r);
+
+        $this->assertCount(2, $xs);
+        $this->assertSame(5.0, $xs[0]);
+        $this->assertSame(5.0, $xs[1]);
+    }
+
+    public function test_a_ray_misses_a_sphere(): void
+    {
+        $r = Ray::from(
+            Tuple::point(0, 2, -5),
+            Tuple::vector(0, 0, 1)
+        );
+
+        $s = Sphere::from(
+            Tuple::point(0, 0, 0),
+            1.0
+        );
+
+        $xs = $s->intersect($r);
+
+        $this->assertEmpty($xs);
+    }
+
+    public function test_a_ray_originates_inside_a_sphere(): void
+    {
+        $r = Ray::from(
+            Tuple::point(0, 0, 0),
+            Tuple::vector(0, 0, 1)
+        );
+
+        $s = Sphere::from(
+            Tuple::point(0, 0, 0),
+            1.0
+        );
+
+        $xs = $s->intersect($r);
+
+        $this->assertCount(2, $xs);
+        $this->assertSame(-1.0, $xs[0]);
+        $this->assertSame(1.0, $xs[1]);
+    }
+
+    public function test_a_sphere_is_behind_a_ray(): void
+    {
+        $r = Ray::from(
+            Tuple::point(0, 0, 5),
+            Tuple::vector(0, 0, 1)
+        );
+
+        $s = Sphere::from(
+            Tuple::point(0, 0, 0),
+            1.0
+        );
+
+        $xs = $s->intersect($r);
+
+        $this->assertCount(2, $xs);
+        $this->assertSame(-6.0, $xs[0]);
+        $this->assertSame(-4.0, $xs[1]);
     }
 }
