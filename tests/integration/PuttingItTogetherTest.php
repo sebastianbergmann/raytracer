551fe87bf220bfb65adeb485646459d8082e7721
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-06-25 09:11:04 +0200

    Better variable name

--- tests/integration/PuttingItTogetherTest.php
+++ tests/integration/PuttingItTogetherTest.php
@@ -15,19 +15,19 @@ final class PuttingItTogetherTest extends TestCase
 {
     public function test_chapter_4(): void
     {
-        $size   = 200;
-        $radius = 75;
-        $black  = Color::from(0.0, 0.0, 0.0);
-        $white  = Color::from(255.0, 255.0, 255.0);
-        $canvas = Canvas::from($size, $size, $black);
-        $twelve = Tuple::point(0, 0, 1);
+        $canvasSize = 200;
+        $radius     = 75;
+        $black      = Color::from(0.0, 0.0, 0.0);
+        $white      = Color::from(255.0, 255.0, 255.0);
+        $canvas     = Canvas::from($canvasSize, $canvasSize, $black);
+        $twelve     = Tuple::point(0, 0, 1);
 
         foreach (range(1, 12) as $hour) {
             $rotation = Matrix::rotationAroundY($hour * (M_PI / 6));
             $point    = $rotation->multiplyBy($twelve);
 
-            $x = (int) round($point->x() * $radius + $size / 2);
-            $y = (int) round($point->z() * $radius + $size / 2);
+            $x = (int) round($point->x() * $radius + $canvasSize / 2);
+            $y = (int) round($point->z() * $radius + $canvasSize / 2);
 
             $canvas->writePixel($x, $y, $white);
         }
