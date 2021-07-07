238db0fcf2bab40bf01f104a89dfcb47ca39c5da
Sebastian Bergmann <sb@sebastian-bergmann.de>
2021-07-07 06:42:11 +0200

    Double resolution by using the LOWER HALF BLOCK (U+2584) Unicode character
    Thanks to @wez and @tobmaster for this idea.

--- src/canvas/AnsiMapper.php
+++ src/canvas/AnsiMapper.php
@@ -14,15 +14,19 @@ final class AnsiMapper
     {
         $buffer = '';
 
-        foreach (range(1, $canvas->height()) as $y) {
+        foreach (range(1, $canvas->height(), 2) as $y) {
             foreach (range(1, $canvas->width()) as $x) {
-                $color = $canvas->pixelAt($x, $y);
+                $bg = $canvas->pixelAt($x, $y);
+                $fg = $canvas->pixelAt($x, $y + 1);
 
                 $buffer .= sprintf(
-                    "\x1b[48;2;%d;%d;%dm  ",
-                    $color->redAsInt(),
-                    $color->greenAsInt(),
-                    $color->blueAsInt(),
+                    "\x1b[48;2;%d;%d;%dm\x1b[38;2;%d;%d;%dm▄",
+                    $bg->redAsInt(),
+                    $bg->greenAsInt(),
+                    $bg->blueAsInt(),
+                    $fg->redAsInt(),
+                    $fg->greenAsInt(),
+                    $fg->blueAsInt(),
                 );
             }
 
