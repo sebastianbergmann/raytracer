82907df87f0aa2e146ca68d7ba6a2f0caaa6a555
Sebastian Bergmann <sb@sebastian-bergmann.de>
2022-04-16 09:16:16 +0200

    Create benchmarks for "Putting it together" examples

---
+++ tests/benchmark/PuttingItTogetherBench.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+namespace SebastianBergmann\Raytracer;
+
+final class PuttingItTogetherBench
+{
+    public function bench_chapter_4(): void
+    {
+        /* @noinspection UnusedFunctionResultInspection */
+        (new PuttingItTogether)->chapter_4();
+    }
+
+    public function bench_chapter_5(): void
+    {
+        /* @noinspection UnusedFunctionResultInspection */
+        (new PuttingItTogether)->chapter_5();
+    }
+
+    public function bench_chapter_6(): void
+    {
+        /* @noinspection UnusedFunctionResultInspection */
+        (new PuttingItTogether)->chapter_6();
+    }
+
+    public function bench_chapter_8(): void
+    {
+        /* @noinspection UnusedFunctionResultInspection */
+        (new PuttingItTogether)->chapter_8();
+    }
+
+    public function bench_chapter_10(): void
+    {
+        /* @noinspection UnusedFunctionResultInspection */
+        (new PuttingItTogether)->chapter_10();
+    }
+}
