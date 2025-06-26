56e04b2e485e4456038c83bc920a31af6241ee7e
Sebastian Bergmann <sb@sebastian-bergmann.de>
2025-06-26 08:12:41 +0200

    Update tools

--- tools/.phpstan/vendor/composer/installed.php
+++ tools/.phpstan/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '148dbf68dceb2a12cd4c68302cfedc2ba59e4e7b',
+        'reference' => '34e76a6482b2310d1db047ff489116c8cc823635',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '148dbf68dceb2a12cd4c68302cfedc2ba59e4e7b',
+            'reference' => '34e76a6482b2310d1db047ff489116c8cc823635',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
